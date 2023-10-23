eaa85a6c5b2646d93221f08c0bc12d313dd0d719
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2023-10-23 14:45:36 -0400

    BST

--- src/myBinarySearchTree.java
+++ src/myBinarySearchTree.java
@@ -1,29 +1,26 @@
-/*
-@author Jere Perisic
-@version October 23, 2023
+/**
+* @author Jere Perisic
+* @version October 23, 2023
+*
+ * A simple binary search tree implementation that stores integers.
  */
 public class myBinarySearchTree extends BinarySearchTree {
+    private Node root;
+    private boolean addReturn;
     public myBinarySearchTree() {
 
     }
-
+    /**
+     * Adds an integer to the binary search tree.
+     *
+     * @param item The integer to be added.
+     * @return true if the item was added, false if it's a duplicate.
+     */
     @Override
-    public boolean add(int data) {
-        if (root == null) {
-            root = new Node(data);
-            getSize();
-            return true;
-        } else {
-            if (addRecursive(root, data)) {
-                getSize();
-                return true;
-            }
-            return false;
-        }
+    public boolean add(int item) {
+        root = addRecursiveHelper(root, item);
+        return addReturn;
     }
 
 
-    private boolean addRecursive(Node current, int data) {
-        pass
-    }
 }
